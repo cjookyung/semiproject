17c8805ddb89ad58856058b6eee241545d048b4c
Choi Jookyung <cjookyung@gmail.com>
2025-06-10 15:19:29 +0900

    갤러리 - 갤러리 리스트

--- src/main/java/org/example/semiproject/gallery/domain/dto/GalleryDTO.java
+++ src/main/java/org/example/semiproject/gallery/domain/dto/GalleryDTO.java
@@ -3,11 +3,11 @@ package org.example.semiproject.gallery.domain.dto;
 import java.time.LocalDateTime;
 
 public interface GalleryDTO {
-    int getBno();
+    int getGgno();
     String getTitle();
     String getUserid();
     LocalDateTime getRegdate();
-    String getSImagName();
+    String getSimgname();
     String getThumbs();
     String getViews();
 }
